c2b58ec7ca98f21d682894105a4fa0b00fdeefcc
Newmusicyy <119541252+Newmusicyy@users.noreply.github.com>
2022-12-01 03:16:17 +0530

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,9 +48,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
-                await self.one.join_chat("FallenAssociation")
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("vivekevil")
+                await self.one.join_chat("NOOBCREATOR")
+                await self.one.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(1)
@@ -79,9 +79,9 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
-                await self.one.join_chat("FallenAssociation")
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("vivekevil")
+                await self.one.join_chat("NOOBCREATOR")
+                await self.one.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(2)
@@ -110,9 +110,9 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
-                await self.one.join_chat("FallenAssociation")
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("vivekevil")
+                await self.one.join_chat("NOOBCREATOR")
+                await self.one.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(3)
@@ -141,9 +141,9 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
-                await self.one.join_chat("FallenAssociation")
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("vivekevil")
+                await self.one.join_chat("NOOBCREATOR")
+                await self.one.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(4)
@@ -172,9 +172,9 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.one.join_chat("IgCutiesClub")
-                await self.one.join_chat("FallenAssociation")
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("vivekevil")
+                await self.one.join_chat("NOOBCREATOR")
+                await self.one.join_chat("NOOBXCREATOR")
             except:
                 pass
             assistants.append(5)
